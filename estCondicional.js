c3518d2aabe953aa7e218d865fecf94ab8565780
ALEJO1205 <thomas.alejo.2002@gmail.com>
2023-08-01 16:39:17 -0300

    INICIO JAVA

---
+++ estCondicional.js
@@ -0,0 +1,8 @@
+//IF...ELSE
+let textUser= prompt("Escriba 'javascript'");
+
+if (textUser === "javascript"){
+    console.log("OK MUY BIEN ESCRITO");
+}else{
+    console.log("DEBES CORREGIR LA ESCRITURA");
+}
